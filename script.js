5ea7dfd0823802395b0f039f4bc06811fd4a3bdf
Mathias <juniormathiias@gmail.com>
2020-03-05 17:34:09 +0000

    added the Conditional and changed the font-family from CSS

--- script.js
+++ script.js
@@ -3,20 +3,14 @@ function button(){
     var randomNumber = Math.floor(Math.random()*6) + 1
     var img = document.getElementById('imgWainting')
     if(number.value.length == 0){
+        alert('Choose the number')
         document.getElementById('result').textContent = `Choose The Number `
     } else  if(randomNumber == number.value) {
         document.getElementById('result').textContent = `How did you know ? `
         img.src = 'surprise.png'
     } else{
-        document.getElementById('result').textContent = `Come on try again`
+        document.getElementById('result').textContent = `Come on try again, I was thinking about the number ${randomNumber}`
         img.src ='happy.png'
     }
-
-   /* if(number.value.length == 0){
-        document.getElementById('result').textContent = `Choose The Number `
-    }else{
-        document.getElementById('result').textContent = `You Chose ${number.value}`
-    }
-*/
 }
 
